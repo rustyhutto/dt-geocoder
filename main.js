73759b7093ae70368e0e4d45274602d62263179a
mbigras <mbigras22@gmail.com>
2016-10-17 19:50:35 -0700

    Implement geocode endpoint

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 require('dotenv').config()
-var express = require('express');
-var app = express();
 
-app.get('/', function (req, res) {
-  // res.send('Hello World!');
-  res.send("meaning of life? " + process.env.CATS)
+var express = require('express');
+var NodeGeocoder = require('node-geocoder');
+
+
+var options = {
+    provider: 'google',
+
+    // Optional depending on the providers
+    httpAdapter: 'https', // Default
+    apiKey: process.env.GM_API_KEY, // for Mapquest, OpenCage, Google Premier
+    formatter: null // 'gpx', 'string', ...
+};
+
+var app = express();
+var geocoder = NodeGeocoder(options);
+
+app.get('/geocode', function(req, res) {
+    // Using callback
+    // Or using Promise
+    geocoder.geocode('29 champs elysée paris')
+        .then(function(response) {
+            console.log(response);
+            res.send(response)
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+    // res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
 });
